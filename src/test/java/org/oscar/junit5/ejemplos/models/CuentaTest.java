a6b18a2c96125d7704703cfe1ba09570c25abbe8
Oscar Ruiz <blackbird3871@gmail.com>
2022-08-25 23:58:34 -0500

    Se utiliza el test condicional @EnabledIfEnvironmentVariable y sus correspondiente disabled para ejecutar o no una prueba

--- src/test/java/org/oscar/junit5/ejemplos/models/CuentaTest.java
+++ src/test/java/org/oscar/junit5/ejemplos/models/CuentaTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.condition.*;
 import org.oscar.junit5.ejemplos.exceptions.DineroInsuficienteException;
 
 import java.math.BigDecimal;
+import java.util.Map;
 import java.util.Properties;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -197,5 +198,31 @@ class CuentaTest {
     @Test
     @EnabledIfSystemProperty(named="ENV", matches="dev")
     void testDev() {
+    }
+
+    @Test
+    void imprimirVariablesAmbiente(){
+        Map<String, String> getenv = System.getenv();
+        getenv.forEach((k, v)->System.out.println(k+"="+v));
+    }
+
+    @Test
+    @EnabledIfEnvironmentVariable(named="JAVA_HOME", matches=".*1.8.0.*")
+    void testJavaHome() {
+    }
+
+    @Test
+    @EnabledIfEnvironmentVariable(named="NUMBER_OF_PROCESSORS", matches="12")
+    void testProcesadores() {
+    }
+
+    @Test
+    @EnabledIfEnvironmentVariable(named="ENVIRONMENT", matches="dev")
+    void testEnv() {
+    }
+
+    @Test
+    @DisabledIfEnvironmentVariable(named="ENVIRONMENT", matches="prod")
+    void testEnvProdDisabled() {
     }
 }
